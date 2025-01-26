b500ee1bbf42a421113d2c2846aa3391fbd7edc0
Xatmo <42683376+Xatmo980@users.noreply.github.com>
2025-01-26 01:14:18 -0800

    Add Options
    Added Options for Ease of Config

--- ladder.php
+++ ladder.php
@@ -2,11 +2,18 @@
 if (!defined('SECURE_ACCESS')) {
     die('Direct access not permitted');
 }
-
-$xmlfile = "path/var/ladders/d2ladder.xml";
+/////////////////////////////////////////////////
+//          Options
+$ladderType = "Expansion"; // Possible Types: Standard, Hardcore, Expansion, Expansion HC
+$GatewayADD = "URL";
+$RegFileLink = "https://URL/Network.reg";
+//
+/////////////////////////////////////////////////
+//          FilePaths
+$xmlfile = "Path/var/ladders/d2ladder.xml";
 $accountsdir = "Path/var/charinfo";
-$xmlfile2 = "path/var/status/server.xml";
-
+$xmlfile2 = "Path/var/status/server.xml";
+//
 /////////////////////////////////////////////////
 
 if(file_exists($xmlfile2)) {
@@ -15,7 +22,7 @@ if(file_exists($xmlfile2)) {
    $hours = $xml2->Uptime->Hours;
    $mins = $xml2->Uptime->Minutes;
    $users = $xml2->Users->Number;
-   print "<center><div class=\"Top\"><center>(How to Connect)<br>Gateway: URL<br><br>Download and run Reg File <a href=\"https://URL\">Download</a><br></center></div></center><br><br>";
+   print "<center><div class=\"Top\"><center>(How to Connect)<br>Gateway: $GatewayADD<br><br>Download and run Reg File <a href=\"$RegFileLink\">Download</a><br></center></div></center><br><br>";
    print "<center><div class=\"Top2\"><center>(Server Uptime)<br> $days(Days) $hours(Hours) $mins(Minutes)<br><br> (Users Online)<br> $users</center></div></center><br><br>";
 }
 
@@ -39,10 +46,10 @@ $anzahl_tote = $anzahl_lebende = 0;
 $xml = simplexml_load_file($xmlfile);
 print "<center><table cellpadding=\"2\" cellspacing=\"2\" style=\"width:70%;border:1px solid black;border-radius:10px;background-color:#2C2F33\"";
 foreach ($xml->ladder as $ladder) {
-         if($ladder->mode  == "Expansion") {
+         if($ladder->mode  == $ladderType) {
             if($ladder->class == "OverAll")  {
 
-	       print " <tr><colspan=\"7\"><font size=\"6\">Expansion Ladder</font></th></tr>\n";
+	       print " <tr><colspan=\"7\"><font size=\"6\">$ladderType Ladder</font></th></tr>\n";
 	       print " <tr><th>#</th><th>Name</th><th>Level</th><th>Exp</th><th>Class</th><th>Title</th><th>Status</th></tr>\n";
 
            foreach($ladder->char as $char) {
@@ -55,9 +62,10 @@ foreach ($xml->ladder as $ladder) {
 		   $status = $char->status;
 		   $acc = $accounts[strtolower($name)];
                                  
-		  if($prefix = " ") {
+			
+                  if($prefix = " ") {
                         $prefix = "None";
-                  }	
+                  }		
 		  if($status == "alive") {
 		        $status = "<font color=\"green\">alive</font>";
 			$anzahl_lebende++;
